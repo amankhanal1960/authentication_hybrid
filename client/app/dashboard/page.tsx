3ff9bdbf3870d0a45ca42271512440305451052e
amankhanal1960 <amankhanal1960@gmail.com>
2025-09-11 23:48:15 +0545

    ::fix::

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-
+import Header from "@/components/header";
 const page = () => {
   return (
-    <div className="p-4 flex flex-col gap-4 backdrop-blur-2xl bg-amber-500">
-      hola konichiwa こんにちは 你好 안녕하세요 مرحبا नमस्ते Bonjour Hallo Ciao
-      Olá Здравствуйте שלום สวัสดี Merhaba Hej Sawubona Halo ola{" "}
+    <div>
+      <Header />
     </div>
   );
 };
