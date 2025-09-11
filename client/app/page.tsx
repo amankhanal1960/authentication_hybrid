f32a037e922134957e9dc0bffb8c4293a6e0989e
amankhanal1960 <amankhanal1960@gmail.com>
2025-09-11 13:54:22 +0545

    ::redirect to auth/register

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,26 +1,5 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return (
-    <div className="flex flex-col gap-4 pt-4 items-center justify-center">
-      <input
-        className=" border-1"
-        type="email"
-        placeholder="Enter email"
-      ></input> 
-      <input
-        className="border-1"
-        type="password"
-        placeholder="emter password"
-      ></input>
-      <button
-        className="px-2 py-1 rounded-sm bg-black text-white"
-        type="submit"
-      >
-        submit
-      </button>
-    </div>
-  );
-};
-
-export default page;
+export default function Home() {
+  redirect("/auth/register"); // redirects immediately
+}
