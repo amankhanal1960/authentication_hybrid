f9167d2a2f5c6a214d23795eaa6da1679ec39a3e
amankhanal1960 <amankhanal1960@gmail.com>
2025-09-20 11:50:28 +0545

    ::url mismathc fix

--- client/services/api.js
+++ client/services/api.js
@@ -1,5 +1,6 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000";
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "https://authenticationserver.vercel.app";
 
 const handleResponse = async (res) => {
   const text = await res.text();
